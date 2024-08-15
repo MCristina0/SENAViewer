95737c8a0f8bc2e65ff42454a730a2f49af5b7af
MCristina0 <juagadorade2018@gmail.com>
2024-08-15 09:10:27 -0500

    book

--- src/edu/misena/senaviewer/model/book.java
+++ src/edu/misena/senaviewer/model/book.java
@@ -1,5 +1,28 @@
 package edu.misena.senaviewer.model;
 
+import java.util.List;
+
 public class book {
+
+    private String id;
+    private String title;
+    private String edititionDate;
+    private String editorial;
+    private List<String> authors;
+    private String isbn;
+    private boolean readed;
+    private int timeReaded;
+
+
+
+    public book(String title, String edititionDate, String editorial, String isbn) {
+        this.title = title;
+        this.edititionDate = edititionDate;
+        this.editorial = editorial;
+        this.isbn = isbn;
+        this.readed = false; // Por defecto, el libro no ha sido leído
+        this.timeReaded = 0; // Por defecto, el tiempo leído es 0
+    }
+
 
 }
